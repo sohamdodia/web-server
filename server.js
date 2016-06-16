c55a32e0c2a29315cc8774b87f479629cb2f3058
Soham Dodia <sohamdodia@gmail.com>
2016-06-16 12:30:45 +0530

    Refactor middleware

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var express = require('express');
 var app=express();
 
-var middleware = {
-	requireAuthentication : function (req,res,next) {
-		console.log('Private route hit!');
-		next();
-	},
-	logger : function(req,res,next) {
-		console.log(req.method);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
+// var middleware = {
+// 	requireAuthentication : function (req,res,next) {
+// 		console.log('Private route hit!');
+// 		next();
+// 	},
+// 	logger : function(req,res,next) {
+// 		console.log(req.method);
+// 		next();
+// 	}
+// };
 
 // app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
-app.get('/about',function (req,res) {
+app.get('/about',middleware.requireAuthentication,function (req,res) {
 	res.send('About us');
 });
 
